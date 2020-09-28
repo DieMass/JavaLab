f80520a66015b0a16899c828d82e7292511b4b1d
diemass <diemass1908@gmail.com>
2020-09-28 21:27:04 +0300

    added first homework

---
+++ 3к1с/rabbitmq-producer/src/main/java/die/mass/rabbitmq/consumers/FirstListener.java
@@ -0,0 +1,34 @@
+package die.mass.rabbitmq.consumers;
+
+import com.google.gson.Gson;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.layout.element.Paragraph;
+import die.mass.rabbitmq.PassportDto;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@EnableRabbit
+@Component
+@AllArgsConstructor
+public class FirstListener {
+
+	private final Gson gson;
+	private final PdfCreator pdfCreator;
+	private final static String queueName = "firstQueue";
+
+	@RabbitListener(queues = queueName)
+	@SneakyThrows
+	public void listen(Message message) {
+		var dto = gson.fromJson(new String(message.getBody()), PassportDto.class);
+		var document = pdfCreator.createPdf("Отпуск", dto.getSurname(), dto.getName());
+		String content = String.format("Прошу предоставить отпуск %s %s на неопределённый срок", dto.getSurname(), dto.getName());
+		document.add(new Paragraph(content).setFont(PdfFontFactory.createFont("src/main/resources/times.ttf","CP1251", true)));
+		document.close();
+	}
+
+}
